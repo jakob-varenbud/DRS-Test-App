6e810bd75ff231999d1da11ab8acf69b47534115
Jakob Varenbud <103386301+jakob-varenbud@users.noreply.github.com>
2023-11-12 22:38:42 +0100

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import './index.css';
-import Navbar from './navbar';
-import HeaderSection from './headersection';
-import MultiStepForm from './multistep';
-
+import "./index.css";
+import Navbar from "./navbar";
+import HeaderSection from "./headersection";
+import MultiStepForm from "./multistep";
 
 function App() {
   return (
@@ -12,7 +11,8 @@ function App() {
       <MultiStepForm />
     </div>
   );
-};
+}
 
 //This is a Test
+//This is a test 2
 export default App;
--- src/headersection.jsx
+++ src/headersection.jsx
@@ -1,25 +1,29 @@
 function HeaderSection() {
-    return (
-      <section className='p-4'>
-        <h1>
-          Smart Repair Hamburg
-          <br />
-          Buchen Sie in 3 Schritten Ihren Smart-Repair Termin
-        </h1>
-        <button className='mt-3'>
-          <a href="#multistep">Jetzt Smart Repair anfragen</a>
-        </button>
-        <button className='mt-2 button-secondary'>
-          <a href="#info">Auftragsstatus einsehen</a>
-        </button>
-        <div className='flex flex-col gap-y-1 mt-5'>
-          <div className='icon-p'>Für Parkschäden, Dellen, Kratzer, Steinschläge und Innenraum</div>
-          <div className='icon-p'>Kostengünstig und schnell</div>
-          <div className='icon-p'>Einfach Foto schicken und zum Festpreis reparieren lassen</div>
+  return (
+    <section className="p-4">
+      <h1>
+        Smart Repair Hamburg
+        <br />
+        Buchen Sie in 3 Schritten Ihren Smart-Repair Termin
+      </h1>
+      <button className="mt-3">
+        <a href="#multistep">Jetzt Smart Repair anfragen</a>
+      </button>
+      <button className="mt-2 button-secondary">
+        <a href="#info">Auftragsstatus einsehen</a>
+      </button>
+      <div className="flex flex-col gap-y-1 mt-5">
+        <div className="icon-p">
+          Für Parkschäden, Dellen, Kratzer, Steinschläge und Innenraum
         </div>
-      </section>
-    );
-  }
-  
-  export default HeaderSection;
-  
+        <div className="icon-p">Kostengünstig und schnell</div>
+        <div className="icon-p">
+          Einfach Foto schicken und zum Festpreis reparieren lassen
+        </div>
+      </div>
+    </section> //Das ist Github
+    //Hallo
+  );
+}
+
+export default HeaderSection;
